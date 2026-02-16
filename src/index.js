88f1a25308c8aa77e0ad4f682561bcb2e4c0e1ed
memesh <clerkalmesh@gmail.com>
2026-02-17 02:54:02 +0700

    limit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { app, server } from "./lib/socket.js";
 const port = process.env.PORT;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "10mb"}));
 app.use(cookieParser());
 app.use(cors({
   origin: 'https://nymousechat.vercel.app', 
